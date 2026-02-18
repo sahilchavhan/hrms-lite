541b770f7e305a4bedc30bd666b02fc0c188139b
sahil chavhan <sahilchavhan57@gmail.com>
2026-02-19 02:44:09 +0530

    sqlite changes

--- backend/hrms_project/settings.py
+++ backend/hrms_project/settings.py
@@ -68,9 +68,7 @@ import dj_database_url
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=not DEBUG,
+        default="sqlite:///db.sqlite3"
     )
 }
 
